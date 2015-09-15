4880f7f5b38c774aa56ff98d251630c9e4369411
Pedro <pedrocch@fanbot.me>
2015-09-15 18:37:25 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- node.php
+++ node.php
@@ -6,32 +6,23 @@
 			if(isset($_GET["name"])){
 		    	$fnbtName  = htmlspecialchars($_GET["name"]);
 				if (findFnbt($fnbtName)) { 	
+
 					if($_SESSION['status'] == 0){
-						$_SESSION['pageNumber'] = 1;
 						$_SESSION['error'] = 2;
-						echo "<script>window.location='resources/library/error.php';</script>";	
-						$_SESSION['pageNumber'] = 1;
-
-					} else {
-
-					$_SESSION['pageNumber'] = 2;				
-					require_once("resources/library/action.php");
-				} else {
+						header("location: ./resources/library/error.php");
+					} else {				
+						require_once("resources/library/action.php");
+				} 
+				
+				}else {
 					$_SESSION['nameErr'] = TRUE;
 					header("location: ./index.php");
 				}			
 			} else if(isset($_GET["code"])){
-			    $_SESSION['pageNumber'] = 3;
+
 			    getUserFbInfo($_GET["code"]);
 				require_once("resources/library/action.php");
 
-					} else {
-						$_SESSION['nameErr'] = TRUE;							
-						echo "<script>window.location='index.php';</script>";	
-
-					}
-				break;
-		    case 2:
 		    } else if(isset($_GET["error"])) {
 			    $_SESSION['pageNumber'] = 3;
 			    $_SESSION['error'] = 0;
