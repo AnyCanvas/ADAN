f52757f77fcc2b6f540e23eb63e8eb2c28bfbc08
Pedro <pedrocch@fanbot.me>
2015-09-24 13:29:27 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- app.php
+++ app.php
@@ -2,8 +2,12 @@
 <head>
 <title>Fb test</title>
 </head>
-<a href="fb:/faceweb/soyfanbot.com">Facebook page</a>
-<a href="fb:/faceweb/(initWithURL:soyfanbot.com)">Facebook page</a>
+<a href="fb:/faceweb/href=http://soyfanbot.com">Facebook page</a>
+<a href="fb:/faceweb/href=soyfanbot.com">Facebook page</a>
+<a href="fb:/faceweb/http://soyfanbot.com">Facebook page</a>
+
+<a href="fb:/faceweb/(initWithURL:http://soyfanbot.com)">Facebook page</a>
+<a href="fb:/faceweb/(http://soyfanbot.com)">Facebook page</a>
 
 <body>
 </body>
