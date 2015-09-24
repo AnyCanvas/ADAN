b7a762ba4355053a9d281c4d42cea62121eac0a9
Pedro <pedrocch@fanbot.me>
2015-09-24 14:33:17 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- app.php
+++ app.php
@@ -2,7 +2,7 @@
 <head>
 <title>Fb test</title>
 </head>
-<a href="fb://faceweb/f?href=https://www.facebook.com/fanbotme">Facebook page</a>
+<a href="fb://page/1550316151894751">Facebook page</a>
 <body>
 </body>
 </html>
