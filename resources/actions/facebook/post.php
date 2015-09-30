a263f6a4683417678e22203731c492c7a26463f5
Pedro <pedrocch@fanbot.me>
2015-09-30 11:06:34 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -45,8 +45,9 @@
 		});		
 
 	function check_in() {
-	  alert( "Handler for .click() called." );
+	  alert(  $('#message').value() );
 	  location.href='<?php echo $loginUrl;?>';
+	  document.cookie="message=" + $('#message').value();
 	};
 	  	
   	</script>
@@ -100,7 +101,7 @@
 		<div class="row mid-row">
 		    <div class="col-xs-1" ></div>
 		    <div class="col-xs-10" style="background-color: white; z-index: 1; padding: 10px; box-shadow: 2px 2px 1px 1px rgba(137,140,144,0.5)">
-			    <input type="text" class="form-control" placeholder="Escribe tu mensaje(opcional)">
+			    <input type="text" class="form-control" id="message" placeholder="Escribe tu mensaje(opcional)">
 			    <div class="iframecontainer" style="background: transparent; z-index: 2; padding-top: 10px;">
 					<div style="z-index: -1;" class="fb-page lockframe" style = "padding: 0px;" data-href="https://www.facebook.com/<?php echo $_SESSION['config']['link']; ?>" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/<?php echo $_SESSION['fbPage']; ?>"><a href="https://www.facebook.com/<?php echo $_SESSION['fbPage']; ?>">Fanbot</a></blockquote></div></div>
 			    </div>
