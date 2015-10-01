c143976b0ac84aca3369cb632ac63bec523f88a3
Pedro <pedrocch@fanbot.me>
2015-10-01 14:27:23 -0500

    *

--- string.php
+++ string.php
@@ -4,8 +4,13 @@ echo $_SERVER['HTTP_USER_AGENT'] . "\n\n";
 $browser = $_SERVER['HTTP_USER_AGENT'];
 
 if (strpos($browser,'Android') !== FALSE){
-	echo "Android\n";
+	echo "\nAndroid\n";
+	if (strpos($browser,'Android 5') !== FALSE){
+		echo "Lollipop";		
+	} else {
+		echo "No lollipop";
+	}
 } else if (strpos($browser,'Iphone') !== FALSE) {
-	echo "Iphone\n";
+	echo "\nIphone\n";
 }
 ?>
