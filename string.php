d9d0ca817a58b4fd41be9db4dff8cec5d25bf4be
Pedro <pedrocch@fanbot.me>
2015-10-01 14:40:42 -0500

    *

--- string.php
+++ string.php
@@ -6,12 +6,12 @@ $browser = $_SERVER['HTTP_USER_AGENT'];
 if (strpos($browser,'Android') !== FALSE){
 	echo "\nAndroid\n";
 	if (strpos($browser,'Android 5') !== FALSE){
-		echo "Lollipop";		
+	echo "Lollipop";		
+	header("location: fb://profile/1550316151894751");
 	} else {
 		echo "No lollipop";
 	}
 } else if (strpos($browser,'iPhone') !== FALSE) {
-	echo("Iphone\n");
 	header("location: fb://profile/1550316151894751");
 }
 ?>
