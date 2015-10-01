b71ebccdf427909c31259779cd07e943441c36ee
Pedro <pedrocch@fanbot.me>
2015-10-01 18:35:55 -0500

    *

--- resources/index/iphone.php
+++ resources/index/iphone.php
@@ -9,7 +9,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<meta charset="utf-8"> 
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap-social.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	<script>
@@ -25,68 +27,28 @@
 </head>
 
 <body>
-	<div class="container-fluid">
-
-	<!-- Fanbot logo columns-->
-	  <div class="row top-row">
-	    <div class="col-xs-3" ></div>
-	    <div class="col-xs-6" >
-	    	<img class="img-responsive" alt="Fanbot" src="media/images/index_iphone.png">
-	    </div>
-	    <div class="col-xs-3" ></div>
-	  </div>
-
-	<div class="clearfix visible-xs-block"></div>
-
-
-	<!-- Text columns-->
-		<div class="row mid-row">
+	<div class ="container-fluid">
+		
+		<!-- Informative image Columns-->
+		  <div class="brand-row row">
 		    <div class="col-xs-1" ></div>
 		    <div class="col-xs-10" >
-				<?php
-						if(isset($_SESSION['nameErr'])){
-						if ($_SESSION['nameErr']){
-							$_SESSION['nameErr'] = FALSE;
-							echo ('<div class="alert alert-danger text-center text-nowrap" role="alert">La Fanbot que buscas no existe.</div>');
-				}
-				}
-				?>
-		    	<p class="text-center text-nowrap" style="color: white; font-size: 1.5em; ">Escribe el nombre de<br>la Fanbot</p>
+		    	<img class="img-responsive" alt="Name help image" src="media/images/index_iphone.png">
 		    </div>
 		    <div class="col-xs-1" ></div>
-		</div>
+		  </div>
 
-	<div class="clearfix visible-xs-block"></div>
+		<div class="clearfix visible-xs-block"></div>
 
-	<!-- Input and button to next page columns-->
-	  <div class="row mid-row">
-	    <div class="col-xs-1" ></div>
-	    <div class="col-xs-10" >
-
-			<form class="form" action="node.php" method="get">
-				<div class="input-group square-box">
-	      			<input type="text" class="form-control input-lg text-center lead text-lowercase inline-text square-box" name="name">
-	      			<span class="input-group-btn">
-	        			<button class="btn btn-lg next-btn square-box" type="submit">
-	        				<span class="glyphicon glyphicon-chevron-right"></span>
-	        			</button>
-					</span>
-	    		</div>
-			</form>
-
-	    </div>
-	    	<div class="col-xs-1" ></div>
-	  </div>
-
-
-	<!-- Informative image Columns-->
-	  <div class="row">
-	    <div class="col-xs-3" ></div>
-	    <div class="col-xs-8" >
-	    	<img class="img-responsive" alt="Name help image" src="media/images/serial.png">
-	    </div>
-	    <div class="col-xs-1" ></div>
-	  </div>
+		<div class="row mid-row">
+		    <div class="col-xs-2" ></div>
+		    <div class="col-xs-8" >
+				<a class="btn btn-block btn-lg btn-social btn-facebook" href="fb://profile/1550316151894751">
+	    			<i class="fa fa-facebook-official"></i> <span class="text-center" style="font-size: 1.1em;" >Continuar</span>
+	  			</a>
+		    </div>
+		    <div class="col-xs-2" ></div>
+		</div>	
 
 	</div>
 </body>
