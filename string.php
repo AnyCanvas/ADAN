63ee1b4f4027488d35dd90ef26edeb25030c1b6e
Pedro <pedrocch@fanbot.me>
2015-10-01 14:23:37 -0500

    *

--- string.php
+++ string.php
@@ -1,3 +1,11 @@
 <?php
 echo $_SERVER['HTTP_USER_AGENT'] . "\n\n";
+
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+if (strpos($a,'Android') !== FALSE){
+	echo "Android\n";
+} else if (strpos($a,'Iphone') !== FALSE) {
+	echo "Iphone\n";
+}
 ?>
