35d3671de106c0a5a9f985e0607a8540f7823222
Pedro <pedrocch@fanbot.me>
2015-10-01 14:35:59 -0500

    *

--- string.php
+++ string.php
@@ -11,6 +11,6 @@ if (strpos($browser,'Android') !== FALSE){
 		echo "No lollipop";
 	}
 } else if (strpos($browser,'Iphone') !== FALSE) {
-	echo 'var newUrl = fb://profile/1550316151894751;window.location.replace(newUrl);';
+	echo '<script>document.location.href = "fb://profile/1550316151894751";</script>';
 }
 ?>
