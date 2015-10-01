7a960a02afef89c3815c2b77a24df541cf6db62a
Pedro <pedrocch@fanbot.me>
2015-10-01 18:24:47 -0500

    *

--- index.php
+++ index.php
@@ -6,9 +6,14 @@ $browser = $_SERVER['HTTP_USER_AGENT'];
 if (strpos($browser,'FBAV') !== FALSE){		
 	require_once("resources/index/legacy.php");
 } else if (strpos($browser,'Android 5') !== FALSE){		
-	header("location: fb://page/1550316151894751");
+/**	header("location: fb:/ /page/1550316151894751"); **/
+
+	require_once("resources/index/android.php");
+
 } else if (strpos($browser,'iPhone') !== FALSE) {
-	header("location: fb://profile/1550316151894751");
+/**	header("location: fb:/ /profile/1550316151894751"); **/
+	require_once("resources/index/iphone.php");
+
 } else {
 	require_once("resources/index/legacy.php");
 	}
--- resources/index/android.php
+++ resources/index/android.php
@@ -31,7 +31,7 @@
 	  <div class="row top-row">
 	    <div class="col-xs-3" ></div>
 	    <div class="col-xs-6" >
-	    	<img class="img-responsive" alt="Fanbot" src="media/images/fanbot.png">
+	    	<img class="img-responsive" alt="Fanbot" src="media/images/index_android.png">
 	    </div>
 	    <div class="col-xs-3" ></div>
 	  </div>
