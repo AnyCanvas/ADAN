9c76c20e0456dbbe165e9431b7620a2ad95e6fbc
Pedro <pedrocch@fanbot.me>
2015-10-02 18:22:40 -0500

    *

--- string.php
+++ string.php
@@ -6,30 +6,36 @@
 </head>
 <body>	
 	<div id="fb-root"></div>
-		<script>(function(d, s, id) {
+		<script>
+			  window.fbAsyncInit = function() {
+			    FB.init({
+			      appId      : '1645168355707000',
+			      xfbml      : true,
+			      version    : 'v2.4'
+			    });
+
+				FB.getLoginStatus(function(response) {
+				  if (response.status === 'connected') {
+				   alert('conectado');
+				    var uid = response.authResponse.userID;
+				    var accessToken = response.authResponse.accessToken;
+				  } else if (response.status === 'not_authorized') {
+				   alert('conectado');
+				  } else {
+				   alert('desconectado');
+				  }
+				 });
+			  };
+			(function(d, s, id) {
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) return;
 			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.4&appId=1645168355707000";
+			js.src = "//connect.facebook.net/es_LA/sdk.js";
 			fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		</script>
 
 <script>
-setTimeout(function(){
-		FB.getLoginStatus(function(response) {
-		  if (response.status === 'connected') {
-		   alert('conectado');
-		    var uid = response.authResponse.userID;
-		    var accessToken = response.authResponse.accessToken;
-		  } else if (response.status === 'not_authorized') {
-		   alert('conectado');
-		  } else {
-		   alert('desconectado');
-		  }
-		 });
-	},1000);
-
 
 </script>
 </body>
