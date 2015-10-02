1cf1f64e4e2322eee749f368641bdc13d9c105ce
Pedro <pedrocch@fanbot.me>
2015-10-02 18:00:26 -0500

    *

--- string.php
+++ string.php
@@ -1,17 +1,26 @@
 <html>
 <head>
 	<title>Inicia sesion en Facebook para continuar</title>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1645168355707000',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script> 
 </head>
 <body>	
-	<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3&appId=1645165719040597";
-			fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-		</script>
+
 <script>
 FB.getLoginStatus(function(response) {
   if (response.status === 'connected') {
