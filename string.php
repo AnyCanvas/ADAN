3229dfa6cccd1c8810e5368c22628d9e965a7bdb
Pedro <pedrocch@fanbot.me>
2015-10-02 18:11:23 -0500

    *

--- string.php
+++ string.php
@@ -23,11 +23,24 @@
 </head>
 <body>	
 <script>
-	FB.ui(
-	 {
-	  method: 'share',
-	  href: 'https://developers.facebook.com/docs/'
-	}, function(response){});
+	setTimeout(function(){
+		FB.getLoginStatus(function(response) {
+		  if (response.status === 'connected') {
+		    // the user is logged in and has authenticated your
+		    // app, and response.authResponse supplies
+		    // the user's ID, a valid access token, a signed
+		    // request, and the time the access token 
+		    // and signed request each expire
+		    var uid = response.authResponse.userID;
+		    var accessToken = response.authResponse.accessToken;
+		  } else if (response.status === 'not_authorized') {
+		    // the user is logged in to Facebook, 
+		    // but has not authenticated your app
+		  } else {
+		    // the user isn't logged in to Facebook.
+		  }
+		 });
+	},2000);
 
 </script>
 </body>
