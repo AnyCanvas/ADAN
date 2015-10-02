9063db8b094fe2f7217e7fb4454375ced2427a1b
Pedro <pedrocch@fanbot.me>
2015-10-02 18:03:58 -0500

    *

--- string.php
+++ string.php
@@ -18,22 +18,11 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
-	FB.getLoginStatus(function(response) {
-	  if (response.status === 'connected') {
-	    // the user is logged in and has authenticated your
-	    // app, and response.authResponse supplies
-	    // the user's ID, a valid access token, a signed
-	    // request, and the time the access token 
-	    // and signed request each expire
-	    var uid = response.authResponse.userID;
-	    var accessToken = response.authResponse.accessToken;
-	  } else if (response.status === 'not_authorized') {
-	    // the user is logged in to Facebook, 
-	    // but has not authenticated your app
-	  } else {
-	    // the user isn't logged in to Facebook.
-	  }
-	 });
+FB.ui(
+ {
+  method: 'share',
+  href: 'https://developers.facebook.com/docs/'
+}, function(response){});
 </script> 
 </head>
 <body>	
