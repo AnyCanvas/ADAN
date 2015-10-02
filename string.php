c2c5e575c29ba71483605bbddbf4ea5d048a3e92
Pedro <pedrocch@fanbot.me>
2015-10-02 18:12:30 -0500

    *

--- string.php
+++ string.php
@@ -26,21 +26,16 @@
 	setTimeout(function(){
 		FB.getLoginStatus(function(response) {
 		  if (response.status === 'connected') {
-		    // the user is logged in and has authenticated your
-		    // app, and response.authResponse supplies
-		    // the user's ID, a valid access token, a signed
-		    // request, and the time the access token 
-		    // and signed request each expire
+		   alert('conectado');
 		    var uid = response.authResponse.userID;
 		    var accessToken = response.authResponse.accessToken;
 		  } else if (response.status === 'not_authorized') {
-		    // the user is logged in to Facebook, 
-		    // but has not authenticated your app
+		   alert('conectado');
 		  } else {
-		    // the user isn't logged in to Facebook.
+		   alert('desconectado');
 		  }
 		 });
-	},2000);
+	},1000);
 
 </script>
 </body>
