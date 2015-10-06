d30794ef2b96f4139147cfed0510a9fe81394a06
Pedro <pedrocch@fanbot.me>
2015-10-06 11:12:56 -0500

    *

--- string.php
+++ string.php
@@ -1,7 +1,25 @@
 <html>
 <head>
-	<title>Inicia sesión en Facebook para continuar</title>
-
+	<title>Bienvenido a Fanbot</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta charset="utf-8"> 
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-65249445-1', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
+</head>
 </head>
 <body>	
 	<div id="fb-root"></div>
@@ -28,7 +46,7 @@
 				   		if (browserAgent.indexOf("Android 5") > -1){
 					   		alert("Lollipop");
 				   		} else{
-					   		alert("No lollipop");
+					   		   $(function(){ $("#includedContent").load("index.php"); });
 				   		}
 				  }
 				 });
@@ -45,4 +63,6 @@
 <script>
 
 </script>
+
+
 </body>
