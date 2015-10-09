8eee6f080837f2d2dcd735c4dad39fd9b8a40b09
Pedro <pedrocch@fanbot.me>
2015-10-09 17:08:16 -0500

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -118,18 +118,6 @@
 		    <div class="col-xs-1" ></div>
 		</div>	
 
-		<div class="row mid-row">
-		    <div class="col-xs-2" ></div>
-		    <div class="col-xs-8" >
-
-				<a class="btn btn-block btn-lg btn-social btn-facebook" href="<?php echo $loginUrl;?> ">
-	    			<i class="fa fa-facebook-official"></i> <span class="text-center" style="font-size: 1.1em;" >Continuar</span>
-	  			</a>
-		    </div>
-		    <div class="col-xs-2" ></div>
-		</div>	
-
-
 		<div class="clearfix visible-xs-block"></div>
 
 		<div class="row mid-row">
