d186806495591266342f71228382d2e59e0fe2fe
Pedro <pedrocch@fanbot.me>
2015-10-23 12:25:30 -0500

    *

--- final.php
+++ final.php
@@ -3,25 +3,27 @@
 
 	require 'resources/library/functions.php';
 
-	
-	if (checkForDuplucatedLike() || $_SESSION['config']['type'] != 'like'){
-		$deviceId = $_SESSION["deviceId"];
-		$accesToken = $_SESSION['accesToken'];
-//		if(fanbotAction( $deviceId, $accesToken)){
-			fanbotAction( $deviceId, $accesToken);
-			saveUserDataToDB();
-//			if ($_SESSION['fbUserId'] !== '120319224983556'){
-			saveInteractionToDB();
-//			}			
-			require_once("resources/library/success.php");
-//		} else {
-//			$_SESSION['error'] = 2;
-//			require_once("resources/library/error.php");			
-//		}
+	if (isset($_SESSION['config']['type']))	{
+		if (checkForDuplucatedLike() || $_SESSION['config']['type'] != 'like'){
+			$deviceId = $_SESSION["deviceId"];
+			$accesToken = $_SESSION['accesToken'];
+	//		if(fanbotAction( $deviceId, $accesToken)){
+				fanbotAction( $deviceId, $accesToken);
+				saveUserDataToDB();
+	//			if ($_SESSION['fbUserId'] !== '120319224983556'){
+				saveInteractionToDB();
+	//			}			
+				require_once("resources/library/success.php");
+	//		} else {
+	//			$_SESSION['error'] = 2;
+	//			require_once("resources/library/error.php");			
+	//		}
+		} 
 	} else {
-		$_SESSION['error'] = 1;
-		require_once("resources/library/error.php");
-	}
+			$_SESSION['error'] = 1;
+			require_once("resources/library/error.php");
+		}
+		
 	session_unset();
 
 ?>
