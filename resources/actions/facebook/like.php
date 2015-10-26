5edbc71cb2031d982f8667b7accf2e488c327128
Pedro <pedrocch@fanbot.me>
2015-10-26 15:32:08 -0600

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -112,6 +112,12 @@
 
 			    <div class="col-xs-2" ></div>
 			    <div class="col-xs-8" >
+
+
+				    <div class="center-block">
+			            <p style="text-align: center;">Aquí aparecerá la imagen de tu pagina.</p>	            
+						<img id="fbImg" src="https://graph.facebook.com/fanbotme/picture" class="img-responsive img-thumbnail center-block" alt="Cinque Terre">
+					</div>
 	
 				    <div class="col-xs-4" ></div>
 				    <div class="col-xs-2" >
