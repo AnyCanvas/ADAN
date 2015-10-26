94393f3a5a74b0ae8b3d33db0bc69ad645851cf4
Pedro <pedrocch@fanbot.me>
2015-10-26 17:11:19 -0600

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -48,41 +48,36 @@
 <body>
 	
 	<div id="fb-root"></div>
-		<script>
-			
-			  window.fbAsyncInit = function() {
-			    FB.init({
-			      appId      : '<?php echo $config["fbApp"]["appId"]; ?>',
-			      xfbml      : true,
-			      version    : 'v2.4'
-			    });
-
-			    FB.Event.subscribe('edge.create', function(targetUrl) {
-			        console.log('Page like.');
-					window.location="<?php echo $loginUrl;?>";
-			    });
-			    FB.Event.subscribe('edge.remove', function(targetUrl) {
-			      console.log("unLike");
-			    });
-			    
-				FB.api(
-				    "/fanbotme",
-				    function (response) {
-				      if (response && !response.error) {
-				        alert(response.name);
-				      }
-				    }
-				);
-
-			  };
-			(function(d, s, id) {
+		<script>(function(d, s, id) {
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) return;
 			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/es_LA/sdk.js";
+			js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3&appId=<?php echo $config["fbApp"]["appId"] ?>";
 			fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		</script>
+		
+<script>
+  window.fbAsyncInit = function() {
+    FB.Event.subscribe('edge.create', function(targetUrl) {
+        console.log('Page like.');
+		window.location="<?php echo $loginUrl;?>";
+    });
+    FB.Event.subscribe('edge.remove', function(targetUrl) {
+      console.log("unLike");
+    });
+			    
+	FB.api(
+	    "/fanbotme",
+	    function (response) {
+	      if (response && !response.error) {
+	        alert(response.name);
+	      }
+	    }
+	);    
+  };
+  
+</script>
 
 		
 	<div class ="container-fluid">
