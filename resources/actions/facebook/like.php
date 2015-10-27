491665d8f1ec2cc10698fe52c9f163d4b361ab71
Pedro <pedrocch@fanbot.me>
2015-10-26 18:11:32 -0600

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -100,7 +100,7 @@
 						</div>
 					</div>
 		
-					<div class="col-xs-7" style="overflow: hidden; text-overflow: ellipsis; white-space:nowrap;" >
+					<div class="col-xs-7" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;" >
 						<p><?php getFbPageName($_SESSION['config']['link']);?><p>
 						<div id="fblike center-block">
 						    <div class="fb-like center-block" data-action="like" data-href="https://www.facebook.com/<?php echo $_SESSION['config']['link'];?>" data-layout="button" data-show-faces="false" data-share="false" >
