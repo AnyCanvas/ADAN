fc39fd9df3b22f612758ca742aca10bb900188a1
Pedro <pedrocch@fanbot.me>
2015-10-28 13:48:19 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -53,7 +53,7 @@
 		FB.ui({
 		  method: 'share',
 		  href: 'https://www.facebook.com/<?php echo $_SESSION['config']['link']; ?>',
-		  display: 'page',
+		  display: 'pag',
 		}, function(response){
             if (response && response.post_id) {
                       console.log('Post was published.');
