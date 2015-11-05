8fbc5f4a76542ea326c185c0ce2796fae24f92a1
Pedro <pedrocch@fanbot.me>
2015-11-05 17:05:46 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -6,7 +6,7 @@
 			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
 			</div>
 	    </div>
-	    <div class="col-xs-6" style="background-image: url("frame.png");" >
+	    <div class="col-xs-6" style="background-image: url('frame.png');" >
 	    	<img class="img-responsive" alt="Fanbot" src="frame.png" style="height: 80%; margin: auto">
 	    	<div
 	    </div>
