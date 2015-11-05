dae29eb48f6641b8774619a08607338a968e5115
Pedro <pedrocch@fanbot.me>
2015-11-05 16:53:29 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -2,7 +2,9 @@
 
 	  <div class="row top-row">
 	    <div class="col-xs-6" >
-		    <iframe width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+		    <div class="embed-responsive embed-responsive-4by3">
+			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+			</div>
 	    </div>
 	    <div class="col-xs-6" >
 	    	<img class="img-responsive" alt="Fanbot" src="frame.png" style="height: 80%">
