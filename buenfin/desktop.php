7991366e8631a762feff4f4867c4e3c50ed7027c
Pedro <pedrocch@fanbot.me>
2015-11-05 16:50:39 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -1,9 +1,9 @@
 	<div class="container-fluid">
 
-	  <div class="row">
+	  <div class="row top-row">
 	    <div class="col-xs-6" ></div>
 	    <div class="col-xs-6" >
-	    	<img class="img-responsive" alt="Fanbot" src="frame.png" style="max-height: 50%">
+	    	<img class="img-responsive" alt="Fanbot" src="frame.png" style="height: 80%">
 	    </div>
 	  </div>
 
