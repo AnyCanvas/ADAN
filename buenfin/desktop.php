a0cb13c5d0c90f5ffd70676f2e4b4f038775bb3c
Pedro <pedrocch@fanbot.me>
2015-11-05 17:07:23 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -6,7 +6,7 @@
 			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
 			</div>
 	    </div>
-	    <div class="col-xs-6" style="background-image: url('frame.png'); background-repeat: no-repeat;" >
+	    <div class="col-xs-6" style="background-image: url('frame.png'); background-repeat: no-repeat; background-position: center;" >
 	    </div>
 	  </div>
 
