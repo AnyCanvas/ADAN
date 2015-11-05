be3d2eaba12d765706a3467fa2e61ed137dd9eb1
Pedro <pedrocch@fanbot.me>
2015-11-05 17:12:42 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -6,8 +6,8 @@
 			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
 			</div>
 	    </div>
-	    <div class="col-xs-6" style="background-image: url('frame.png'); background-repeat: no-repeat; background-position: center; height: auto; width: auto" >
-		    jajaj
+	    <div class="col-xs-6" >
+		    <div id="cellphone" style="background-image: url('frame.png'); background-repeat: no-repeat; background-position: center; height: 500px; width: 500px"></div>
 	    </div>
 	  </div>
 
