f508d416484f4645fadc45127af5302e46067430
Pedro <pedrocch@fanbot.me>
2015-11-06 17:44:31 -0600

    *

--- resources/library/functions.php
+++ resources/library/functions.php
@@ -104,11 +104,11 @@
 
 		
 		switch ($colorArray['result']){
-			case '1': $texto = fopen('.../buenfin/amarilla.txt', "r");
+			case '1': $texto = fopen('../../buenfin/amarilla.txt', "r");
 				break;
-			case '2': $texto = fopen('.../buenfin/azul.txt', "r");
+			case '2': $texto = fopen('../../buenfin/azul.txt', "r");
 				break;
-			case '3': $texto = fopen('.../buenfin/verde.txt', "r");
+			case '3': $texto = fopen('../../buenfin/verde.txt', "r");
 				break;
 			default;
 				break;
