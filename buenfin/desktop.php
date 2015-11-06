87dae8af9eec9fed8ceec81dac3fd8f5a4cee1b9
Pedro <pedrocch@fanbot.me>
2015-11-06 11:40:59 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -1,6 +1,5 @@
 	<div class="container-fluid">
 
-	  <div class="row top-row">
 	    <div class="col-xs-6" >
 		    <div class="embed-responsive embed-responsive-4by3 center-block" style="margin:auto;">
 			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
@@ -12,7 +11,6 @@
 		    </div>
 	    	<div
 	    </div>
-	  </div>
 
 	</div>
 	
