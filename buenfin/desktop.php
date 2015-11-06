69fb95b4050ce256d1bf8ca7e147055e29cf5b25
Pedro <pedrocch@fanbot.me>
2015-11-06 11:23:03 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -8,7 +8,7 @@
 	    </div>
 	    <div class="col-xs-6">
 		    <div class="center-block" style="height: 627px; width: 375px; background-color: #5daef9; border: black solid">
-			<iframe width="375" height="627" src="../index.php"></iframe>
+			<iframe width="375" height="627" src="../index.php" style="border: none;"></iframe>
 		    </div>
 	    	<div
 	    </div>
