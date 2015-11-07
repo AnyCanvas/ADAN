f6d5140c02e7cef035ed66b5a6c615dd1081d462
Pedro <pedrocch@fanbot.me>
2015-11-06 18:19:40 -0600

    *

--- final.php
+++ final.php
@@ -15,7 +15,7 @@
 				saveUserDataToDB();
 				saveInteractionToDB();
 				require_once("resources/library/success.php");	
-				sendMail($texto);			
+//				sendMail($texto);			
 // Fin de agregado por medio de internet.
 			} else {
 				fanbotAction( $deviceId, $accesToken);
