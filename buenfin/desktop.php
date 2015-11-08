c403659b6e106714258c09f616f9f97265503b42
Pedro <pedrocch@fanbot.me>
2015-11-08 11:20:31 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -2,7 +2,17 @@
 
 	    <div class="col-xs-6" >
 		    <div class="embed-responsive embed-responsive-16by9" style="margin:auto; padding-top: 5em;">
-			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/S4HSB5fIi_o"" frameborder="0" allowfullscreen></iframe>
+			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/S4HSB5fIi_o" frameborder="0" allowfullscreen></iframe>
+			    	<div class="clearfix visible-xs-block"></div>
+
+
+		<div class="row mid-row">
+		    <div class="col-xs-1" ></div>
+		    <div class="col-xs-10 text-center leadinline-text" >
+				<span style="color: white; font-size: 1.5em; ">¿Como puedo usar Fanbot en mi negocio?</span>
+		       <a type="button" class="btn btn-info btn-lg" href="https://www.facebook.com/1550316151894751/posts/1625343501058682">Ve a la publicación y pregunta.</a>   
+		    </div>
+		</div>
 			</div>
 	    </div>
 	    <div class="col-xs-6">
