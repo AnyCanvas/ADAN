0764fb93ebbee74b163ba515b55e8b4bbde94aa1
Pedro <pedrocch@fanbot.me>
2015-11-08 11:20:56 -0600

    *

--- buenfin/desktop.php
+++ buenfin/desktop.php
@@ -3,7 +3,9 @@
 	    <div class="col-xs-6" >
 		    <div class="embed-responsive embed-responsive-16by9" style="margin:auto; padding-top: 5em;">
 			    <iframe class="embed-responsive-item" width="420" height="315" src="https://www.youtube.com/embed/S4HSB5fIi_o" frameborder="0" allowfullscreen></iframe>
-			    	<div class="clearfix visible-xs-block"></div>
+			    
+			</div>
+				<div class="clearfix visible-xs-block"></div>
 
 
 		<div class="row mid-row">
@@ -13,7 +15,6 @@
 		       <a type="button" class="btn btn-info btn-lg" href="https://www.facebook.com/1550316151894751/posts/1625343501058682">Ve a la publicación y pregunta.</a>   
 		    </div>
 		</div>
-			</div>
 	    </div>
 	    <div class="col-xs-6">
 		    <div class="center-block" style="height: 633px; width: 381px; background-color: #5daef9; border: black solid">
