7bc59874894a8e2e882fdd62edaea386f21a5c5f
Pedro <pedrocch@fanbot.me>
2015-11-12 17:31:06 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -60,6 +60,15 @@
 		                    }
 				});
 			}	
+		   window.fbAsyncInit = function() {
+		    FB.Event.subscribe('edge.create', function(targetUrl) {
+		        console.log('Page like.');
+				window.location="<?php echo $loginUrl;?>";
+		    });
+		    FB.Event.subscribe('edge.remove', function(targetUrl) {
+		      console.log("unLike");
+		    });
+		  };
   
 </script>
 
