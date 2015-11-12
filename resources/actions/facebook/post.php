fb2668cc28f0a99bdee0cd077834930c166a9250
Pedro <pedrocch@fanbot.me>
2015-11-12 17:33:53 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -61,7 +61,7 @@
 				});
 			}	
 		   window.fbAsyncInit = function() {
-		    FB.Event.subscribe('edge.create', function(targetUrl) {
+		    FB.Event.subscribe('edge.send', function(targetUrl) {
 		        console.log('Page like.');
 				window.location="<?php echo $loginUrl;?>";
 		    });
