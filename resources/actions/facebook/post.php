61cd23cc3bec40f522ea3297cb5edbd9c7167cf7
Pedro <pedrocch@fanbot.me>
2015-11-12 17:03:45 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -50,7 +50,7 @@
 				FB.ui({
 				  method: 'share',
 				  href: 'https://www.facebook.com/<?php echo $_SESSION['config']['link']; ?>',
-				  display: 'async',
+				  display: 'page',
 				}, function(response){
 		            if (response && response.post_id) {
 		                      console.log('Post was published.');
