5c519e7112116c965e4aea7dd52ff0de10e4e4fd
Pedro <pedrocch@fanbot.me>
2015-11-12 17:53:46 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -60,15 +60,6 @@
 		                    }
 				});
 			}	
-		   window.fbAsyncInit = function() {
-		    FB.Event.subscribe('message.send', function(targetUrl) {
-		        console.log('Page like.');
-				window.location="<?php echo $loginUrl;?>";
-		    });
-		    FB.Event.subscribe('edge.remove', function(targetUrl) {
-		      console.log("unLike");
-		    });
-		  };
   
 </script>
 
@@ -92,7 +83,9 @@
 		<div class="row mid-row">
 		    <div class="col-xs-2" ></div>
 		    <div class="col-xs-8" >
-			<div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button"></div>
+				<a id="post-btn" class="btn btn-block btn-lg btn-social btn-facebook" href="https://m.facebook.com/dialog/share?+app_id=1645165719040597+&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F+&redirect_uri=<?php echo $loginUrl;?>">
+	    			<i class="fa fa-facebook-official"></i> <span class="text-center" style="font-size: 1.1em;" >Continuar</span>
+	  			</a>
 		    </div>
 		    <div class="col-xs-2" ></div>
 		</div>	
