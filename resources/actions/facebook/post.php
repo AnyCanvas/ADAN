bb6b4d6da4706ac0fb4b58155b632edf52b95cb0
Pedro <pedrocch@fanbot.me>
2015-11-24 13:13:03 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -113,9 +113,6 @@
   </div>
 	<script>
 		$('#actionModal').modal('show').css({
-		    'margin-top': function () { //vertical centering
-		        return -($(this).height() / 2);
-		    },
 		    'margin-left': function () { //Horizontal centering
 		        return -($(this).width() / 2);
 		    }
