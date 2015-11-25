3618c73150191484f9c3fa5f2dae7eb660fef5b2
Pedro <pedrocch@fanbot.me>
2015-11-24 18:31:35 -0600

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -74,7 +74,7 @@
 		    <div class="col-xs-4" ></div>
 		    <div class="col-xs-4" >
 
-				<img class="img-responsive center-block" src="media/images/fanbot.png" alt="fanbot" width="200"/>
+				<img class="img-responsive center-block" src="media/images/	<?php  echo $_SESSION['config']['image']; ?>" alt="fanbot" width="200"/>
 		    </div>
 		    <div class="col-xs-4" ></div>
 	</div>
