624a1587c88f60e0b369365f4feb99ce064cb1e8
Pedro <pedrocch@fanbot.me>
2015-11-26 10:54:44 -0600

    *

--- resources/index/index.php
+++ resources/index/index.php
@@ -30,13 +30,13 @@
 
 	<div class="clearfix visible-xs-block"></div>
 	<div class="col-xs-1"></div>
-	<div class="col-xs-10">
+	<div class="col-xs-10" style="padding: 0px;">
 		<div id="indexDiv" class="center-block">
-			<div class="col-xs-5" style="padding-right: 0px;">
+			<div class="col-xs-5" style="padding: 0px;">
 				<img src="media/images/maquina.png" class="img-responsive center-block" alt="maquina" style="height: 160px;" />		
 			</div>
 
-		<div class="col-xs-7" style="padding-left: 0px;">
+		<div class="col-xs-7" style="padding: 0px;">
 			<form class="form" action="node.php" method="get" style="padding-top: 50px; padding-left: 0px;">
 				<div class="input-group square-box">
 	      			<input type="text" class="form-control input-lg text-center lead text-lowercase inline-text square-box" name="name">
