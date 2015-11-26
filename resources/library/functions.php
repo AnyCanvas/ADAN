0542f99c6bf1cd2ea39d7efc73bcb5b77f54ef87
Pedro <pedrocch@fanbot.me>
2015-11-26 17:55:07 -0600

    *

--- resources/library/functions.php
+++ resources/library/functions.php
@@ -27,7 +27,7 @@
 		
 	function getFbPageName($page){
 		$pageJson = file_get_contents('https://graph.facebook.com/'. $page .'?access_token=1498446833779418|6Uo2HajAgYUiIE0x8DR1AXuhxbw');
-		$pageArray = json_decode($pageJson, true);
+		$pageArray = json_decode($pageJson);
 		echo $pageArray['name'];
 	}
 
