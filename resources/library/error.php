f7c41e853349ecf32dda6d784b58703d40231e49
Pedro <pedrocch@fanbot.me>
2015-11-26 17:10:40 -0600

    *

--- resources/library/error.php
+++ resources/library/error.php
@@ -32,7 +32,7 @@
 	<!-- Fanbot logo columns-->
 	  <div class="row top-row">
 	    <div class="col-xs-12" >
-	    	<img class="center-block" alt="Fanbot" style="width: 300px;" src="../../media/images/<?php switch ($_SESSION['error']){
+	    	<img class="center-block" alt="Fanbot" style="width: 200px;" src="../../media/images/<?php switch ($_SESSION['error']){
 				case 0:
 		    	   echo 'upss.png';
 				   break;
