b9a6e64750b31e85c3a39fddef2258a4234a7177
Pedro <pedrocch@fanbot.me>
2015-12-01 18:35:56 -0600

    *

--- index.php
+++ index.php
@@ -20,6 +20,40 @@
 </head>
 <body>	
 	<div id="fb-root"></div>
+		<script>
+			  window.fbAsyncInit = function() {
+			    FB.init({
+			      appId      : '<?php echo $config["fbApp"]["appId"]; ?>',
+			      xfbml      : true,
+			      version    : 'v2.4'
+			    });
+
+			FB.getLoginStatus(function(response) {
+			  if (response.status === 'connected') {
+			    ga('send', 'event', "step 1", 'facebook login status', 'logged');
+			    console.log('logged');
+			    var uid = response.authResponse.userID;
+			    var accessToken = response.authResponse.accessToken;
+			  } else if (response.status === 'not_authorized') {
+			    console.log('logged');
+			    ga('send', 'event', "step 1", 'facebook login status', 'logged');
+			  }	else{
+			    console.log('not logged');
+			    ga('send', 'event', "step 1", 'facebook login status', 'not loged');							  
+			  }	
+			});
+  };
+
+			(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/es_LA/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		</script>
+
+	<div id="fb-root"></div>
 	<script> 
 		window.fbAsyncInit = function() {
 					    FB.init({
@@ -28,21 +62,7 @@
 					      version    : 'v2.4'
 					    });
 		
-						FB.getLoginStatus(function(response) {
-						  if (response.status === 'connected') {
-						    ga('send', 'event', "step 1", 'facebook login status', 'logged');
-						    console.log('logged');
-						    var uid = response.authResponse.userID;
-						    var accessToken = response.authResponse.accessToken;
-						  } else if (response.status === 'not_authorized') {
-						    console.log('logged');
-						    ga('send', 'event', "step 1", 'facebook login status', 'logged');
-						  }	else{
-						    console.log('not logged');
-						    ga('send', 'event', "step 1", 'facebook login status', 'not loged');							  
-						  }	
-						});
-			  };
+
 		
 	</script>
        <div class="container-fluid">
