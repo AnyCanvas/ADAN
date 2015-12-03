682bf510a130d69de6830b63403fe473fe627204
Pedro <pedrocch@fanbot.me>
2015-12-03 17:27:53 -0600

    *

--- index.php
+++ index.php
@@ -136,11 +136,10 @@
 		    <div class="modal-content">
 		      <div class="modal-header">
 			      <div class="row">
-     				<div class="col-xs-1"></div>
-				  	<div class="col-xs-2">
+				  	<div class="col-xs-2 col-sm-offset-1">
 				      	<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true" style="font-size: xx-large; text-align: center;"></span>					  	
 				  	</div>
-				  	<div class="col-xs-8">
+				  	<div class="col-xs-10 col-sm-offset-3 col-sm-8">
 					  	<p class="modal-title text-center">No haz iniciado sesión de Facebook en el navegador</p>
 				  	</div>
                     <div class="col-xs-1"></div>
