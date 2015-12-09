ebe57e1394f4a40c1fe2830cbbeabc6a8550a75b
Pedro <pedrocch@fanbot.me>
2015-12-09 16:23:36 -0600

    *

--- resources/library/success.php
+++ resources/library/success.php
@@ -18,7 +18,7 @@
 <body <?php if($_SESSION['id'] == "PF-B1-LTM-0001"){ echo 'style="background-color: black;"'; }?>>
 	<div class="container-fluid">
 
-	 <?php if($_SESSION['id'] != "FB-B1-SCM-00102"){ ?>
+	 <?php if($_SESSION['id'] != "PF-B1-LTM-0001"){ ?>
 	  <div class="center-block" style="width: 220px; height: 415px; padding-top: 10%">
 	    	<img class="img-responsive" alt="Fanbot" src="media/images/done.png">
 	    	<div id="actionNumber">
