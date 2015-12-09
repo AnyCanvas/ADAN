a41ea516bf94fea397aa0bac1dd683227448830c
Pedro <pedrocch@fanbot.me>
2015-12-09 16:11:59 -0600

    *

--- resources/library/error.php
+++ resources/library/error.php
@@ -26,8 +26,10 @@
 				}
 			?>
 </script>
-<body>
+<body <?php if($_SESSION['id'] == "FB-B1-SCM-00102"){ echo 'style="background-color: black;"'; }?>>
 	<div class="container-fluid">
+
+	 <?php if($_SESSION['id'] != "FB-B1-SCM-00102"){ ?>
 
 	<!-- Fanbot logo columns-->
 	  <div class="center-block" style="width: 220px; height: 415px; padding-top: 50%">
@@ -45,6 +47,13 @@
 				}
 			?>">
 	  </div>
+
+	  <?php } else {?>
+	  <div class="center-block" style="width: 220px; height: 415px; padding-top: 10%; color: white; font-size: xx-large; text-align: center;">
+	  		Ocurrió un problema.
+		   	</div>
+	  </div>	  
+	  <?php }?>
 
 	</div>
 </body>
