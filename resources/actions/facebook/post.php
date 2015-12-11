437941fc37bc59abd529cac0613a9e1f3a69c9f6
Pedro <pedrocch@fanbot.me>
2015-12-11 13:59:26 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -39,14 +39,23 @@
 <body>
 	
 	<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.5&appId=<?php echo $config["fbApp"]["appId"] ?>";
-			fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-			
+	<script>
+		window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '<?php echo $config["fbApp"]["appId"] ?>',
+		      xfbml      : true,
+		      version    : 'v2.5'
+		    });
+		  };
+		
+		  (function(d, s, id){
+		     var js, fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) {return;}
+		     js = d.createElement(s); js.id = id;
+		     js.src = "//connect.facebook.net/en_US/sdk.js";
+		     fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+
 			window.fbAsyncInit = function() {
 				FB.Event.subscribe('xfbml.render', finished_rendering);
 			};
