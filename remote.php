23c8bc9340a9125d1ed9d4aa0f8b9f1b3217b9bd
Pedro <pedrocch@fanbot.me>
2015-12-14 12:35:02 -0600

    *

--- remote.php
+++ remote.php
@@ -2,7 +2,7 @@
 
 	require 'resources/library/functions.php';
 	session_start();
-	$fnbtName = 'lol';
+	$fnbtName = $_GET["name"];
 
 	findFnbt($fnbtName);
 	fanbotAction( $_SESSION['deviceId'], $_SESSION['accesToken'] );
