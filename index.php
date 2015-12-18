33ada46cb33778f3f9d2711af07c58bd7a400390
Pedro <pedrocch@fanbot.me>
2015-12-18 16:44:03 -0600

    *

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
                 <div class="col-xs-10">
                     <p class="text-center text-nowrap"
                        style="color: white; font-size: 1.2em;">
-                        Escribe el nombre de la Fanbot
+                        Escribe la palabra de la etiqueta azul
                     </p>
                 </div>
                 <div class="col-xs-1"></div>
