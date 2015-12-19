041796e5cb71025535c829bf4cc3217160ea5940
Pedro <pedrocch@fanbot.me>
2015-12-18 18:00:41 -0600

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -99,6 +99,11 @@
 				    <?php if($_SESSION['id'] == "PF-B1-LTM-0001"){ ?>
 					<img id="fb_img" src="../../../media/clients/logos/centinela.png" class="img-responsive center-block" alt="Logo">
 				    <?php } else { ?>
+					<img id="fb_img" src="<?php 
+					$pictureJson = file_get_contents('https://graph.facebook.com/'. $_SESSION['config']['link'] .'/picture?redirect=false&height=300');
+					$pictureArray = json_decode($pictureJson, true);
+					echo $pictureArray['data']['url'];
+						?> " class="img-responsive img-thumbnail center-block" alt="Logo">
 					<?php } ?>
 				</div>
 		    </div>
