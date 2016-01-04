dc590c2cbb85441ff745e3d634f2123da4379d79
Pedro <pedrocch@fanbot.me>
2016-01-04 12:11:27 -0600

    *

--- resources/html/login.php
+++ resources/html/login.php
@@ -63,13 +63,13 @@
 	  <div class="modal-dialog modal-sm">		
 <?php if($_SESSION['fnbt']['id'] == "PF-B1-LTM-0001"){ ?>
 			  <div class="fb_logo-row row">
-			    <div class="col-xs-3" ></div>
-			    <div class="col-xs-6" >
+			    <div class="col-xs-1" ></div>
+			    <div class="col-xs-10" >
 				    <div class="center-block" >
 						<img class="img-responsive" alt="Fanbot" src="../../media/clients/centinela/clasicoshot.png">
 					</div>
 			    </div>
-			    <div class="col-xs-3" ></div>
+			    <div class="col-xs-1" ></div>
 			  </div>
 	
 	
