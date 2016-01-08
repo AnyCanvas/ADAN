1612453d4b34296ee6b5029342fb58ace1da67e9
Pedro <pedrocch@fanbot.me>
2016-01-07 18:29:22 -0600

    *

--- resources/library/functions.php
+++ resources/library/functions.php
@@ -98,8 +98,8 @@
 
 		$token = json_decode($codeToToken );
 
-		$pageJson = file_get_contents('https://graph.facebook.com/'. $_SESSION['fnbt']['config']['link'] .'?fields=can_checkin&access_token=1498446833779418|6Uo2HajAgYUiIE0x8DR1AXuhxbw');
-		$pageArray = json_decode($pageJson, true);	
+//		$pageJson = file_get_contents('https://graph.facebook.com/'. $_SESSION['fnbt']['config']['link'] .'?fields=can_checkin&access_token=1498446833779418|6Uo2HajAgYUiIE0x8DR1AXuhxbw');
+//		$pageArray = json_decode($pageJson, true);	
 //		error_log($pageArray['can_checkin']);
 		// Get new fb session
 		if (!isset($session)) {
