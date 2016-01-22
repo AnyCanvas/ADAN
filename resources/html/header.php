3507f15385f59b55474da6071ffada789d109f7b
Pedro <pedrocch@fanbot.me>
2016-01-22 14:06:42 -0600

    *

--- resources/html/header.php
+++ resources/html/header.php
@@ -2,7 +2,7 @@
   <html lang="en">
       <meta charset="utf-8"> 
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
 
       <!--Let browser know website is optimized for mobile-->
@@ -10,6 +10,6 @@
 
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/materialize.js"></script>
       <script src="js/common.js"></script>
       <base href=" <?php echo $config["urls"]["baseUrl"]; ?> " target="_self">
