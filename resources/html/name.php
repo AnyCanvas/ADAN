292f582b0daf99d04b451f826e14cdb51d3b0e16
Pedro <pedrocch@fanbot.me>
2016-01-22 18:07:51 -0600

    *

--- resources/html/name.php
+++ resources/html/name.php
@@ -12,7 +12,7 @@
 		</div>
 	    <footer style="height: 25%;">
 		      <p class="fnbt-name-text grey-text">Escribe la palabra de la etiqueta Azul</p>
-		      <form action="node.php" method="get" name="name" onfocus="$('#name-button').show();" onblur="$('#name-button').hide();">
+		      <form action="node.php" method="get" name="name" >
 		      <input type="text" id="fnbt-name-label" class="fnbt-name-input white-text"></input>
 		      <form>
 			<a class="fnbt-name-button button-div btn-floating btn-large waves-effect green accent-2" 
