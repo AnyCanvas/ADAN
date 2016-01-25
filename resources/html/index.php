d9c7c1e2cd5d351262218cf34c66d5aaf07b59f4
Pedro <pedrocch@fanbot.me>
2016-01-25 16:45:10 -0600

    *

--- resources/html/index.php
+++ resources/html/index.php
@@ -7,25 +7,62 @@
 	<script>
 		window.fbAsyncInit = function() {
 		    FB.init({
-		      appId      : '<?php echo $config["fbApp"]["appId"] ?>',
+		      appId      : '<?php echo $config["fbApp"]["appId"]; ?>',
 		      xfbml      : true,
-		      version    : 'v2.5'
+		      version    : 'v2.4'
 		    });
-
-		  };
+		var referrer = document.referrer;	
+		var browserAgent = navigator.userAgent
+		console.log(referrer);	
+		console.log(browserAgent);
+		FB.getLoginStatus(function(response) {
+		  if (response.status === 'connected') {
+		    ga('send', 'event', "step 1", 'facebook login status', 'logged');
+		    console.log('logged');
+		    var uid = response.authResponse.userID;
+		    var accessToken = response.authResponse.accessToken;
+		  } else if (response.status === 'not_authorized') {
+		    console.log('logged');
+		    ga('send', 'event', "step 1", 'facebook login status', 'logged');
+		  }	else{
+		    console.log('not logged');
+		    ga('send', 'event', "step 1", 'facebook login status', 'not logged');	
+			  				
+			if(referrer.indexOf("facebook") <= -1 ){
+				if (browserAgent.indexOf("iPhone") > -1){
+					console.log("iPhone detected");
+					$('#indexModal').modal('show');
+					document.location.href = 'openapp.php';
+					// modalButton.setAttribute('href', 'fb://profile/1550316151894751');
+				} else if (browserAgent.indexOf("Android") > -1){
+					if (browserAgent.indexOf("Android 5") > -1 || browserAgent.indexOf("Android 6") > -1){
+						console.log("Android lollipop detected");
+						document.location.href = 'openapp.php';	
+//						modalButton.setAttribute('href', 'fb://page/1550316151894751');
+						$('#indexModal').modal('show');
+					} else if (browserAgent.indexOf("Android 4.4") > -1){
+						console.log("Android Kitkat detected");
+					} else {
+						console.log("Old Android detected");
+				   	}
+			  	} else {
+					console.log("Model not detected");
+			  	}
+			 }
+		}	
+			});
+			};
+		(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/es_LA/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
 		
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "//connect.facebook.net/en_US/sdk.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
-
-			postclick = function () {
-				window.location = "<?php echo $loginUrl;?>";										
-			}	
-  
+		postclick = function () {
+			window.location = "<?php echo $loginUrl;?>";										
+		}	
 	</script>
 
 	<div class="container-fluid" style="height: 100%; width: 100%">
