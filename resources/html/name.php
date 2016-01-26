770665b016c0ea8c4d5670a49567d16166ef36dd
Pedro <pedrocch@fanbot.me>
2016-01-26 17:43:14 -0600

    *

--- resources/html/name.php
+++ resources/html/name.php
@@ -20,17 +20,6 @@
 	    </footer>
 
 	</div>
-
-	<script>
-			var browserAgent = navigator.userAgent;
-			console.log(browserAgent);
-			if (browserAgent.indexOf("iPhone") > -1){
-				console.log("Changed class");
-				$( "#upper-div" ).addClass( "iphone-fix" );
-				$( "#name-btn" ).addClass( "button-div-iphone-fix" );
-
-			}
-	</script>
     </body>
 
   </html>
