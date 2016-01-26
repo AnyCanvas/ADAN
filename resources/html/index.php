ea62fd9fc0a36d4aa4fad9d19970f6032fb20cc1
Pedro <pedrocch@fanbot.me>
2016-01-26 11:48:00 -0600

    *

--- resources/html/index.php
+++ resources/html/index.php
@@ -77,7 +77,7 @@
 	<div class="container-fluid" style="height: 100%; width: 100%">
 		
 		<div id="loader" style="display: block; width: 100%; height: 100%; z-index: 9; position: absolute; background-color: rgba(0, 0, 0, 0.51);">
-			<div class="cssload-wrapper vertical-center">
+			<div class="wrapper vertical-center">
 				<div class="cssload-loader btn-centered" style="z-index: 10; top: 45vh; margin: auto;"></div>
 			</div>
 		</div>	
