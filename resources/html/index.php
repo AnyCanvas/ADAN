a8cbb698a5c0a8e75ab0e6a5abfc69459271508e
Pedro <pedrocch@fanbot.me>
2016-01-26 17:27:02 -0600

    *

--- resources/html/index.php
+++ resources/html/index.php
@@ -74,9 +74,9 @@
 		}	
 	</script>
 
-	<div class="container-fluid" style="height: 100%; width: 100%">
+	<div class="container-fluid" style="height: 80%; width: 100%">
 		
-		<div id="loader" style="display: block; width: 100%; height: 100%; z-index: 9; position: absolute; background-color: rgba(0, 0, 0, 0.51);">
+		<div id="loader" style="display: block; width: 100%; height: 80%; z-index: 9; position: absolute; background-color: rgba(0, 0, 0, 0.51);">
 			<div class="wrapper vertical-center">
 				<div class="cssload-loader btn-centered" style="z-index: 10; top: 45vh; margin: auto;"></div>
 			</div>
