6937a2da5362558cac143b08c6a8433b98bba5de
Pedro <pedrocch@fanbot.me>
2016-01-28 17:02:42 -0600

    *

--- resources/html/login.php
+++ resources/html/login.php
@@ -1,4 +1,4 @@
-<title>Cargando</title>
+<title>Inicia sesión</title>
 
 	
    <body>
@@ -40,11 +40,14 @@
 			if(referrer.indexOf("facebook") <= -1 ){
 				if (browserAgent.indexOf("iPhone") > -1){
 					console.log("iPhone detected");
+//					$('#indexModal').modal('show');
 					document.location.href = 'openapp.php';
+					// modalButton.setAttribute('href', 'fb://profile/1550316151894751');
 				} else if (browserAgent.indexOf("Android") > -1){
 					if (browserAgent.indexOf("Android 5") > -1 || browserAgent.indexOf("Android 6") > -1){
 						console.log("Android lollipop detected");
 						document.location.href = 'openapp.php';	
+//						modalButton.setAttribute('href', 'fb://page/1550316151894751');
 						$('#indexModal').modal('show');
 					} else if (browserAgent.indexOf("Android 4.4") > -1){
 						console.log("Android Kitkat detected");
@@ -78,10 +81,14 @@
 				<div class="cssload-loader btn-centered" style="z-index: 10; top: 45vh; margin: auto;"></div>
 			</div>
 		</div>	
-		<div id="upper-div" style="height: 100vh; width: 100vw;" class="blue">
-
+		<div id="upper-div" style="height: 74vh; width: 100vw;" class="blue">
+		<div class="div-wrapper full login-img">
 		</div>
 		</div>
+	    <footer style="height: 24vh; width: 100vw;">
+		      <p class="fnbt-name-text grey-text">Es tu primera ves con Fanbot<br>¡Disfruta la experiencia!<p>
+			  <a class="waves-effect waves-light btn fb-btn btn-centered" style=" background-color: #405A9F; font-size: 3vw;" onclick="postclick();"><i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Continuar con facebook</a>
+	    </footer>
 
 	</div>
 	
