72d5ea712b29ac8871f855a12a84edc81d95942b
Pedro <pedrocch@fanbot.me>
2016-02-03 12:04:50 -0600

    *

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$_SESSION['page'] = 1;
+	$_SESSION['page'] = 0;
 	include 'resources/library/functions.php'; 
     $loginUrl = 'https://www.facebook.com/dialog/oauth?client_id='.$config["fbApp"]["appId"].'&redirect_uri='.$config["urls"]["baseUrl"].'/node.php&scope=public_profile,email&response_type=code';	
 
