80e84434b49734eb6d9b03450559cdfd932882ba
Pedro <pedrocch@fanbot.me>
2016-02-03 13:37:42 -0600

    *

--- resources/html/login.php
+++ resources/html/login.php
@@ -23,7 +23,7 @@
 		</div>
 	    <footer style="height: 24vh; width: 100vw;">
 		      <p class="fnbt-name-text grey-text">Es tu primera ves con Fanbot<br>¡Disfruta la experiencia!<p>
-			  <a class="waves-effect waves-light btn fb-btn btn-centered" style=" background-color: #405A9F; font-size: 3vw;" onclick="postclick();"><i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Continuar con facebook</a>
+			  <a class="waves-effect waves-light btn fb-btn btn-centered" style=" background-color: #405A9F; font-size: 3vw;" onclick="postclick();"><i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Continuar con Facebook</a>
 	    </footer>
 
 	</div>
