a2d07415d6fb6d4bc7a7cb246e1107d9fb041e73
Pedro <pedrocch@fanbot.me>
2016-02-05 17:18:08 -0600

    *

--- node.php
+++ node.php
@@ -29,10 +29,13 @@
     					break;
 
 				    case 1:
-				    	if(isset($_SESSION['fnbt']['name'])){
+				    	if( isset($_SESSION['fnbt']['name']) ){
 					    	if(isset($_GET["token"])){
 						    	$tokenArray['access_token'] = $_GET["token"];
 						    	$token = $object = json_decode(json_encode($tokenArray), FALSE);			    	
+						    	getUserFbInfo($token);
+	 							$_SESSION['page'] = 2;
+	    					    require_once("resources/html/name.php");					    		 							
 						    } else if(isset($_GET["code"])){
 						    	$token = fbCode2token($_GET["code"]);
 						    	getUserFbInfo($token);
