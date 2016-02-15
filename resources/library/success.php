3fe604b9136da4824839f821af34646fbda27f09
Pedro <pedrocch@fanbot.me>
2016-02-15 17:02:46 -0600

    *

--- resources/library/success.php
+++ resources/library/success.php
@@ -27,7 +27,7 @@
 		<div id="upper-div" style="height: 75%;" class="blue">
 		<div class="div-wrapper full">
 			<img class="center-img success-img" src="images/success.png">
-			<p><?php echo timeStamp(); ?></p>
+			<p class="center-align " style="z-index: 2; position: relative; bottom: 46%;"><?php echo timeStamp(); ?></p>
 		</div>
 		</div>
 	    <footer style="height: 25%; padding-top: 17.5vh;" class="blue">
