708acf16e5710f9d16cb96678ffee7c1a2c48ff0
Pedro <pedrocch@fanbot.me>
2016-02-14 22:41:58 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -14,7 +14,7 @@
 			<img class="center-img fbpage-img" src="https://graph.facebook.com/<?php echo $_SESSION['fnbt']['config']['link'];?>/picture?type=large" class="img-responsive img-thumbnail center-block" alt="Cinque Terre">
 			
 		<div class="center-align white-text" style="margin-top: 46%;">
-			Bienvenido a Wingman<br>Obtén recomensas por cada visita.
+			Bienvenido a Wingman<br><span style="font-weight: light;">Obtén recomensas por cada visita.</span>
 		</div>
 
 		</div>
