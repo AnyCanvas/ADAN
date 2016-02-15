22afce496eff2d39cd94691d58f5847772ab7118
Pedro <pedrocch@fanbot.me>
2016-02-14 22:49:23 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -26,22 +26,21 @@
 			  			<i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Check-in
 			  		</a>
 			  		
-				  		<p style="font-size: x-small; text-decoration: underline;" onclick="$('#modal1').openModal();">Prefiero utilizar el código de mi recibo<p>
+			  		<p style="font-size: x-small; text-decoration: underline;" onclick="$('#modal1').openModal();">Prefiero utilizar el código de mi recibo<p>
 
 			  </div>
 	    </footer>
-	    
-	    <!-- Modal Structure -->
-	  <div id="modal1" class="modal modal-fixed-footer">
-	    <div class="modal-content">
-	      <h4>Modal Header</h4>
-	      <p>A bunch of text</p>
-	    </div>
-	    <div class="modal-footer">
-	      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
-	    </div>
-	  </div>
 
+		<!-- Modal Structure -->
+		  <div id="modal1" class="modal bottom-sheet">
+		    <div class="modal-content">
+		      <h4>Modal Header</h4>
+		      <p>A bunch of text</p>
+		    </div>
+		    <div class="modal-footer">
+		      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+		    </div>
+		  </div>
 	</div>
 	<script>
 			var browserAgent = navigator.userAgent;
