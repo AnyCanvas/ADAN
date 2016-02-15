9cd3446ffb8060dd36fb1b0419af0a7560b2d95d
Pedro <pedrocch@fanbot.me>
2016-02-14 22:45:40 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -26,10 +26,23 @@
 			  			<i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Check-in
 			  		</a>
 			  		
-			  		<p style="font-size: x-small; text-decoration: underline;" onclick="alert('click');">Prefiero utilizar el código de mi recibo<p>
+			  		<a class="modal-trigger waves-effect waves-light btn" href="#modal1">
+				  		<p style="font-size: x-small; text-decoration: underline;" onclick="alert('click');">Prefiero utilizar el código de mi recibo<p>
+					</a>
 
 			  </div>
 	    </footer>
+	    
+	    <!-- Modal Structure -->
+	  <div id="modal1" class="modal modal-fixed-footer">
+	    <div class="modal-content">
+	      <h4>Modal Header</h4>
+	      <p>A bunch of text</p>
+	    </div>
+	    <div class="modal-footer">
+	      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
+	    </div>
+	  </div>
 
 	</div>
 	<script>
