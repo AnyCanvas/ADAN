eaaf43e138d68731ba3af906f8e4aa8fa80db008
Pedro <pedrocch@fanbot.me>
2016-02-15 13:17:53 -0600

    *

--- resources/html/header.php
+++ resources/html/header.php
@@ -4,6 +4,7 @@
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link type="text/css" rel="stylesheet" href="/css/materialize.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="/css/style.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="//cdn.materialdesignicons.com/1.4.57/css/materialdesignicons.min.css">
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, height=device-height, minimum-scale=1.0001, initial-scale=1.0001, maximum-scale=1.0001, user-scalable=no">
