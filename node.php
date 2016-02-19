ccf3b1a394df5726238337176c5a1b094d0dfcd2
Pedro <pedrocch@fanbot.me>
2016-02-19 11:11:12 -0600

    *

--- node.php
+++ node.php
@@ -55,15 +55,16 @@
 
 						if( isset($_GET["name"])){
 						    $fnbtName  = htmlspecialchars($_GET["name"]);								
-
-							if($_SESSION['fnbt']['name'] == 'chappy'){
-								require_once("resources/actions/facebook/develop.php");									
-							} else if (findFnbt($fnbtName)) {								
-								if($_SESSION['fnbt']['status'] == 0){
+							if (findFnbt($fnbtName)) {
+								
+								if($_SESSION['fnbt']['name'] == 'chappy'){
+									$_SESSION['page'] = 3;
+									require_once("resources/actions/facebook/develop.php");									
+								} else if($_SESSION['fnbt']['status'] == 0){
 									$_SESSION['error'] = 2;
 									$_SESSION['page'] = 0;
 									header("location: ./resources/library/error.php");
-								} else if( ! (fanbotStatus($_SESSION['fnbt']["deviceId"], $_SESSION['fnbt']['accesToken']) ) ){
+								} else if( !(fanbotStatus($_SESSION['fnbt']["deviceId"], $_SESSION['fnbt']['accesToken']) ) ){
 									$_SESSION['error'] = 1;
 									$_SESSION['page'] = 0;
 									require_once("resources/html/error2.php");									
