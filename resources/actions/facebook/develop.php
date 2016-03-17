49867fceca359cdc38d60f678381ae7210fea1ca
Pedro <pedrocch@fanbot.me>
2016-03-17 17:03:16 -0600

    *

--- resources/actions/facebook/develop.php
+++ resources/actions/facebook/develop.php
@@ -29,13 +29,13 @@
 		</div>
 		</div>
 	    <footer style="height: 25%;">
-		      <p class="fnbt-name-text grey-text">Comprueba tu visita con un Check-in<br>o introduce el código de tu recibo.<p>
+		      <p class="fnbt-name-text grey-text">Comprueba tu visita con un Check-in<br>o introduce el código de tu recibo. entro<p>
 			  <div class="like-div" style="overflow: hidden;">
 		  			<a class="waves-effect waves-light btn fb-btn btn-centered" style=" background-color: #405A9F; font-size: 3vw;" onclick="postclick();">
 			  			<i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Check-in con Facebook
 			  		</a>
 			  		
-			  		<p style="font-size: x-small; text-decoration: underline;" onclick="$('#modal1').openModal();">Prefiero utilizar el código de mi recibo entro</p>
+			  		<p style="font-size: x-small; text-decoration: underline;" onclick="$('#modal1').openModal();">Prefiero utilizar el código de mi recibo</p>
 
 			  </div>
 	    </footer>
