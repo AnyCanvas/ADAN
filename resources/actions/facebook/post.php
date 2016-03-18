5f89f1f62c5a58b30268e4b5fcefc7abf60f498a
Pedro <pedrocch@fanbot.me>
2016-03-18 11:43:24 -0600

    *

--- resources/actions/facebook/post.php
+++ resources/actions/facebook/post.php
@@ -20,7 +20,7 @@
 		</div>
 		</div>
 	    <footer style="height: 25%;">
-		      <p class="fnbt-name-text grey-text">Comprueba tu visita con un Check-in<br>o introduce el código de tu recibo.<p>
+		      <p class="fnbt-name-text grey-text">Comprueba tu visita con un Check-in.<p>
 			  <div class="like-div" style="overflow: hidden;">
 		  			<a class="waves-effect waves-light btn fb-btn btn-centered" style=" background-color: #405A9F; font-size: 3vw;" onclick="postclick();">
 			  			<i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Check-in con Facebook
