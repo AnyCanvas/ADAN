ef4f22e2b2677f543805bd567937424d4cce4f20
Pedro <pedrocch@fanbot.me>
2016-03-31 16:40:52 -0600

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -81,7 +81,6 @@
 				  			<i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Check-in con Facebook
 				  		</a>
 				  </div>
-				  <p style="font-size: x-small; text-align: center; padding-top: 5px;">Al continuar estarás aceptando el <a href="http://fanbot.me/aviso-de-privacidad/" target="_blank">Aviso de Privacidad</a></p>
 		      </div>
 <?php } ?>
 	    </footer>
