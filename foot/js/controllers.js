5b2d25a0922d12d70e46222b50478ca9a0c48874
Pedro <pedrocch@fanbot.me>
2016-06-27 13:30:16 -0500

    *

--- foot/js/controllers.js
+++ foot/js/controllers.js
@@ -43,6 +43,16 @@ angular.module('app.controllers', [])
    
 .controller('codigoCtrl', function($scope, $location, localStorageService) {
 	console.log($location.search().token);
+	// To add to local storage
+	  localStorageService.set('localStorageKey','Add this!');
+	  // Read that value back
+	  var value = localStorageService.get('localStorageKey');
+	  // To remove a local storage
+	  localStorageService.remove('localStorageKey');
+	  // Removes all local storage
+	  localStorageService.clearAll();
+	  // You can also play with cookies the same way
+	  localStorageService.cookie.set('localStorageKey','I am a cookie value now');
 })
    
 .controller('likeCtrl', function($scope) {
