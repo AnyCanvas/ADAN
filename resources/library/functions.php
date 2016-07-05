0f6ce097b572ea4da021dded75aed610a20af99f
Pedro <pedrocch@fanbot.me>
2016-07-05 15:50:40 -0500

    *

--- resources/library/functions.php
+++ resources/library/functions.php
@@ -55,7 +55,7 @@
 		$_SESSION['fbUser']['lastName'] = $me->getLastName();
 		$_SESSION['fbUser']['gender'] = $me->getGender();
 
-		print_r($me->getField('friends'));
+		print($me->getField('friends'));
  //		$_SESSION['fbUser']['friends'] = $me->getGraphNode()->getField('friends');
 
 	}
