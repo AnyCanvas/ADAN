55c3dbd67682ef08665abacd947cef41e5c9f77c
Pedro <pedrocch@fanbot.me>
2016-07-27 14:56:08 -0500

    *

--- resources/actions/surveys/rate.php
+++ resources/actions/surveys/rate.php
@@ -64,7 +64,7 @@
 		</div>
 	    <footer style="height: 25%;">
 			  <div id="fblike-div">
-			      <p class="fnbt-name-text grey-text">Presiona “Me Gusta”<br>para accionar la máquina.</p>
+			      <p class="fnbt-name-text grey-text">Contesta para jugar<br>para accionar la máquina.</p>
 				  <div class="like-div" style="overflow: hidden;">
 				  		<input id="rating-input" type="number" value="3" />
 				  </div>
