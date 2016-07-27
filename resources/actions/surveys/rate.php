7e4f501c7a0b7081b606441cc68b780d220f9b0c
Pedro <pedrocch@fanbot.me>
2016-07-27 14:53:38 -0500

    *

--- resources/actions/surveys/rate.php
+++ resources/actions/surveys/rate.php
@@ -84,6 +84,42 @@
 				$("#fbpost-div").show();
 				$("#fblike-div").hide();
 			}
+			
+    jQuery(document).ready(function () {
+        $("#input-21f").rating({
+            starCaptions: function(val) {
+                if (val < 3) {
+                    return val;
+                } else {
+                    return 'high';
+                }
+            },
+            starCaptionClasses: function(val) {
+                if (val < 3) {
+                    return 'label label-danger';
+                } else {
+                    return 'label label-success';
+                }
+            },
+            hoverOnClear: false
+        });
+        
+        $('#rating-input').rating({
+              min: 0,
+              max: 5,
+              step: 1,
+              size: 'xl',
+              showClear: false,
+              showCaption: false
+           });
+                   
+        $('#rating-input').on('rating.change', function() {
+            alert($('#rating-input').val());
+        });
+        
+        
+        $('.rb-rating').rating({'showCaption':true, 'stars':'3', 'min':'0', 'max':'3', 'step':'1', 'size':'xs', 'starCaptions': {0:'status:nix', 1:'status:wackelt', 2:'status:geht', 3:'status:laeuft'}});
+    });
 	</script>
     </body>
   </html>
