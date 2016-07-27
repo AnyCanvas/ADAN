5125ff80f94f49451bc721802e62665f8345cafd
Pedro <pedrocch@fanbot.me>
2016-07-27 17:25:06 -0500

    *

--- resources/actions/surveys/rate.php
+++ resources/actions/surveys/rate.php
@@ -86,24 +86,7 @@
 			}
 			
     jQuery(document).ready(function () {
-        $("#input-21f").rating({
-            starCaptions: function(val) {
-                if (val < 3) {
-                    return val;
-                } else {
-                    return 'high';
-                }
-            },
-            starCaptionClasses: function(val) {
-                if (val < 3) {
-                    return 'label label-danger';
-                } else {
-                    return 'label label-success';
-                }
-            },
-            hoverOnClear: false
-        });
-        
+
         $('#rating-input').rating({
               min: 0,
               max: 5,
@@ -113,12 +96,10 @@
               showCaption: false
            });
                    
-        $('#rating-input').onclick('rating.change', function() {
+        $('#rating-input').on('rating.change', function() {
             console.log($('#rating-input').val());
         });
-        
-        
-        $('.rb-rating').rating({'showCaption':true, 'stars':'3', 'min':'0', 'max':'3', 'step':'1', 'size':'xs', 'starCaptions': {0:'status:nix', 1:'status:wackelt', 2:'status:geht', 3:'status:laeuft'}});
+
     });
 	</script>
     </body>
