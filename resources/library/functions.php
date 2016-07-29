c2b03a9a50a20f984d21a852f3add9c4634cc6c2
Pedro <pedrocch@fanbot.me>
2016-07-29 12:22:43 -0500

    *

--- resources/library/functions.php
+++ resources/library/functions.php
@@ -268,7 +268,7 @@
 				if ( $_SESSION['fnbt']['config']['type'] == 'rate' ){
 					$_SESSION['fnbt']['data'] = json_decode($row["survey"],true);
 					
-					$n = idate("is") % 4;
+					$n = idate("U") % 4;
 
 					switch ($n) {
 						case 0:
