7b542506027513e5fd18023f2c01c0460bf3cd9e
Pedro <pedrocch@fanbot.me>
2016-07-29 11:53:42 -0500

    *

--- final.php
+++ final.php
@@ -11,14 +11,14 @@
 
 	if (isset($_SESSION['fnbt']['config']))	{
 		if ( $_SESSION['error'] != "name" ){
-			require_once("resources/library/success.php");
-			$deviceId = $_SESSION['fnbt']["deviceId"];
-			$accesToken = $_SESSION['fnbt']['accesToken'];
-//			fanbotAction( $deviceId, $accesToken);
 			if($_SESSION['action'] == 'rate'){
 				$_SESSION['data'] = "{'q':.'". $_SESSION['q'] ."','a':'". $_GET['a'] ."'}";
 				print_r($_SESSION['data']);
 			}
+			require_once("resources/library/success.php");
+			$deviceId = $_SESSION['fnbt']["deviceId"];
+			$accesToken = $_SESSION['fnbt']['accesToken'];
+//			fanbotAction( $deviceId, $accesToken);
 			saveInteractionToDB();
 		} else {
 				require_once("resources/html/error1.php");
