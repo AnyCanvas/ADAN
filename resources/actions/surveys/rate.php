27da80f4d13ae6a6468e4c7e8d59c507add3c106
Pedro <pedrocch@fanbot.me>
2016-08-03 12:02:34 -0500

    *

--- resources/actions/surveys/rate.php
+++ resources/actions/surveys/rate.php
@@ -119,11 +119,11 @@
 				  </p>
 				  <div class="like-div" style="overflow: hidden;">
 					<fieldset class="rating">
-					    <input type="radio" id="star5" name="rating" value="5" onclick="setTimeout(function() {rate('5')},500)" /><label for="star5" title="Awesome - 5 stars"></label>
-					    <input type="radio" id="star4" name="rating" value="4" onclick="setTimeout(function() {rate('4')},500)" /><label for="star4" title="Pretty good - 4 stars"></label>
-					    <input type="radio" id="star3" name="rating" value="3" onclick="setTimeout(function() {rate('3')},500)" /><label for="star3" title="Meh - 3 stars"></label>
-					    <input type="radio" id="star2" name="rating" value="2" onclick="setTimeout(function() {rate('2')},500)" /><label for="star2" title="Kinda bad - 2 stars"></label>
-					    <input type="radio" id="star1" name="rating" value="1" onclick="setTimeout(function() {rate('1')},500)" /><label for="star1" title="Sucks big time - 1 star"></label>
+					    <input type="radio" id="star5" name="rating" value="5" onclick="setTimeout(function() {rate('5')},500)" /><label for="star5" title=" 5 stars"></label>
+					    <input type="radio" id="star4" name="rating" value="4" onclick="setTimeout(function() {rate('4')},500)" /><label for="star4" title="4 stars"></label>
+					    <input type="radio" id="star3" name="rating" value="3" onclick="setTimeout(function() {rate('3')},500)" /><label for="star3" title="3 stars"></label>
+					    <input type="radio" id="star2" name="rating" value="2" onclick="setTimeout(function() {rate('2')},500)" /><label for="star2" title="2 stars"></label>
+					    <input type="radio" id="star1" name="rating" value="1" onclick="setTimeout(function() {rate('1')},500)" /><label for="star1" title="1 star"></label>
 					</fieldset>
 				  </div>
 		      </div>
