8d9386574bde40b1f50f992344e8a831c7d61169
Pedro <pedrocch@fanbot.me>
2016-08-03 11:52:54 -0500

    *

--- resources/actions/surveys/rate.php
+++ resources/actions/surveys/rate.php
@@ -112,11 +112,6 @@
 			  <div id="fblike-div">
 			      <p class="fnbt-name-text grey-text" lang="es" >Contesta para jugar<br>&iquest;<?php echo($_SESSION['q']); ?>?</p>
 				  <div class="like-div" style="overflow: hidden;">
-
-<div class="valign-wrapper">
-  <h5 class="valign">This should be vertically aligned</h5>
-</div>
-
 					<fieldset class="rating">
 					    <input type="radio" id="star5" name="rating" value="5" onclick="setTimeout(function() {rate('5')},500)" /><label for="star5" title="Awesome - 5 stars"></label>
 					    <input type="radio" id="star4" name="rating" value="4" onclick="setTimeout(function() {rate('4')},500)" /><label for="star4" title="Pretty good - 4 stars"></label>
