64f9b09209899fb48931dbe60ea41630efbafeaa
Pedro <pedrocch@fanbot.me>
2016-08-11 16:43:46 -0500

    *

--- resources/html/name.php
+++ resources/html/name.php
@@ -5,12 +5,12 @@
 		}
 	</script>
 	<div class="container-fluid" style="height: 100%; width: 100%">
-		<div id="upper-div" style="height: 75%;" class="blue">
+		<div id="upper-div" style="height: 65%;" class="blue">
 		<div class="div-wrapper full" style="padding-top: top: 40vh !important;">
 			<img class="center-align center-img index-img" src="images/fanbot_label.png"  />
 		</div>
 		</div>
-	    <footer style="height: 25%;">
+	    <footer style="height: 35%;">
 		      <p class="fnbt-name-text grey-text">Escribe la palabra de la etiqueta Azul</p>
 		      <form action="node.php" method="get" >
 		      <input onfocus="makeBig();" onfocusout="makeSmall();" name="name" id="fnbt-name-label" class="fnbt-name-input white-text"></input>
