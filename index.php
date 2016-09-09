84054c483c9c7bd19c53fb31f0467619d6a7acca
Pedro <pedrocch@fanbot.me>
2016-09-09 17:25:20 -0500

    *

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
 	}
 	$_SESSION['page'] = 0;
 	$_SESSION['error'] = 'no';
-    $loginUrl = 'https://www.facebook.com/dialog/oauth?client_id='.$config["fbApp"]["appId"].'&redirect_uri='.$config["urls"]["baseUrl"].'/node.php&scope=public_profile,user_friends,email&response_type=code';	
-
 	?>
 <!DOCTYPE html>
 <html>
