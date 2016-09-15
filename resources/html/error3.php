19ab01f30dcd0169fa87c60eb1539600736bfa45
Pedro <pedrocch@fanbot.me>
2016-09-15 09:54:58 -0500

    *

--- resources/html/error3.php
+++ resources/html/error3.php
@@ -10,7 +10,7 @@
 		</div>
 	    <footer style="height: 25%;" class="red accent-2">
 		      <p class="error-text white-text">Esta Fanbot está suspendida,<br>puedes probar Fanbot en otro negocio.</p>
-			  <a class="waves-effect waves-light btn white black-text btn-centered" href="fanbot.me/radar
+			  <a class="waves-effect waves-light btn white black-text btn-centered" href="http://fanbot.me/radar
 ">BUSCAR OTROS NEGOCIOS</a>
 	    </footer>
 
