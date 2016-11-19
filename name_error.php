8db9f143bd650ea61c1b43cc232df5814d43f0b8
Pedro <casillaschavezjuanpedro@gmail.com>
2016-11-18 21:38:48 -0600

    *

--- name_error.php
+++ name_error.php
@@ -12,8 +12,8 @@
  
     <body>
 
-	<div class="container-fluid red accent-2" style="height: 100%; width: 100%">
-		<div id="upper-div" class="red accent-2" style=" height: 75%; background-image: url(./images/error.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto 60%;">
+	<div class="container-fluid" style="height: 100%; width: 100%">
+		<div id="upper-div" class="red accent-2" style=" height: 75%; background-image: url(./images/error.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto 60%; background-color: #ee5559;">
 		</div>
 	    <footer style="height: 25%;" class="red accent-2">
 		      <p class="error-text white-text">Esa palabra no coincide<br>con la de la etiqueta azul.<p>
@@ -21,13 +21,5 @@
 	    </footer>
 
 	</div>
-	<script>
-			var browserAgent = navigator.userAgent;
-			console.log(browserAgent);
-			if (browserAgent.indexOf("iPhone") > -1){
-				console.log("Changed class");
-
-			}
-	</script>
     </body>
 </html>
